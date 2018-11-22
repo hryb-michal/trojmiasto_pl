c08573c085810f60e3412de3a0d97c359f2a84be
hryb-michal <hryb.michal@gmail.com>
2018-11-22 21:24:08 +0100

    not even working yet

--- konkurs_trojmiasto_pl.py
+++ konkurs_trojmiasto_pl.py
@@ -8,18 +8,20 @@ This is a temporary script file.
 import urllib2
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-#from selenium.webdriver import Firefox
-#from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 
-#quote_page = "https://konkursy.trojmiasto.pl/konkurs-14234/"
-#page = urllib2.urlopen(quote_page)
-#soup = BeautifulSoup(page, 'html.parser')
+#driver = webdriver.Firefox()
 
-driver = webdriver.Firefox()
+main_page = "https://konkursy.trojmiasto.pl/"
+page = urllib2.urlopen(main_page)
+soup = BeautifulSoup(page, 'html.parser')
 
+contests = soup.find_all('li', attrs={'class':' item'})
+contests.append(soup.find('li', attrs={'class': ' first_in_row item'}))
+for contest in contests:
+    #print contest.text
+    print contest.attrs['onclick'][17:-1]
 
-#driver.get("https://duckduckgo.com")
 '''
 driver.get("https://rozrywka.trojmiasto.pl/Przemek-Dyakowski-Nie-jestem-zapiekly-na-cokolwiek-n129478.html")
 elem = driver.find
@@ -35,11 +37,13 @@ elem.submit()
 #search_form = browser.find_element_by_id('search_form_input_homepage')
 #results = browser.find_elements_by_class_name('result_title')
 
-
+'''
 page = driver.get("https://rozrywka.trojmiasto.pl/Przemek-Dyakowski-Nie-jestem-zapiekly-na-cokolwiek-n129478.html")
 soup = BeautifulSoup(page, 'html.parser')
 name_box = soup.find("input", attrs={"type": "checkbox"})
 print name_box.text
+'''
+
 #question_box = soup.find('div', attrs={'class':'question'})
 #question = question_box.find('p', attrs={'class':'frame'})
 #question_text = question.text
