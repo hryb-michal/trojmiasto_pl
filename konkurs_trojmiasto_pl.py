c780241fbfeec268201f7fa017c12dc40062baa6
Michal Hryb <Michal.Hryb@ctm.gdynia.pl>
2018-11-26 10:54:12 +0100

    looping contest recognition

--- konkurs_trojmiasto_pl.py
+++ konkurs_trojmiasto_pl.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
 This is a temporary script file.
 """
 
@@ -12,8 +11,6 @@ from bs4 import BeautifulSoup
 import time
 from functions import to_utf
 #from datetime import datetime
-
-#TODO measure application time
 
 main_address = "https://konkursy.trojmiasto.pl/"
     
@@ -29,21 +26,33 @@ while(no_contests):
     number_of_contests = len(contests)
     
     if (number_of_contests < 1):
-        print "no contests available"
-        time.sleep(2)        
+        print ("no contests available", time.localtime()[3:6])
+        time.sleep(60)        
     else:
         no_contests = False
-    print (after - before)
+        print ("contest found", time.localtime()[3:6])
+    #print (after - before)
 
 print (number_of_contests, ' contest(s) available')
 for contest in contests:
+    contests_names = contest.find_all('p', attrs={'class':'name'})
+    for contest_name in contests_names:
+        print contest_name.text
     print contest.attrs['onclick'][17:-1]
 
+if (len(contests) > 0):
+    for contest in contests:
+        contest_address = contest.attrs['onclick'][17:-1]  #TODO add possibility of contest choice
+        contest_page = urllib2.urlopen(contest_address)
+        contest_soup = BeautifulSoup(contest_page, 'html.parser')
+        time_message_box = contest_soup.find('div', attrs={'class': 'message'})
+        print time_message_box.find('h2').text
+        
+
 ''' #TODO only when there's at least 1 contest
-contest_address = contests[-1].attrs['onclick'][17:-1]  #TODO add possibility of contest choice
+contest_address = contests[1].attrs['onclick'][17:-1]  #TODO add possibility of contest choice
 contest_page = urllib2.urlopen(contest_address)
 contest_soup = BeautifulSoup(contest_page, 'html.parser')
-
 question_box = contest_soup.find('div', attrs={'class':'question'})
 question = question_box.find('p', attrs={'class':'frame'})
 print(question.text)
@@ -52,10 +61,6 @@ print(question.text)
 #TODO check time of the contest
 #current_time = datetime.now().time()
 
-#time_message_box = contest_soup.find('div', attrs={'class': 'message'})
-#time_message.find('h2').text
-
-#for word in 
 
 #time_message_text = time_message_box.fi
 
@@ -70,12 +75,12 @@ elem.clear()
 name_box = driver.find_element_by_name("name")
 name_box.clear()
 name_box.send_keys(to_utf("Michał"))
-surname_box = driver.find_element_by_name("name")
+surname_box = driver.find_element_by_name("surname")
 surname_box.clear()
 surname_box.send_keys(to_utf("Hryb"))
-
-#TODO fill mail box
-
+mail_box = driver.find_element_by_name("email")
+mail_box.clear()
+mail_box.send_keys(to_utf("hryb.michal@gmail.com"))
 checkbox1 = driver.find_element_by_name("rodo[pytania][11]")
 checkbox1.click()
 checkbox2 = driver.find_element_by_name("rodo[pytania][14]")
@@ -84,5 +89,4 @@ checkbox2.click()
 '''
 elem.submit()
 '''
-#driver.close()
-
+#driver.close() 
