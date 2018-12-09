ae429bda487f16f8b1a95a25e45e76223c8e1c2f
hryb-michal <hryb.michal@gmail.com>
2018-12-09 13:07:53 +0100

    delete comments

--- functions.py
+++ functions.py
@@ -5,11 +5,19 @@ Created on Sat Nov 24 14:01:46 2018
 
 @author: hrymimakos
 """
+import urllib2
+from bs4 import BeautifulSoup
 
 def to_utf(string_to_convert):
     return unicode(string_to_convert, "utf-8", errors="ignore")
 
-def time_of_start(time_to_start):
-    for word in time_to_start:  #not actually word, it's letters
-        if (word == "mniej"):
-            return False
+def list_contests(contests):
+    for contest in contests:
+        contest_name = contest.find('p', attrs={'class':'name'})
+        contest_address = contest.attrs['onclick'][17:-1]
+        contest_page = urllib2.urlopen(contest_address)
+        contest_soup = BeautifulSoup(contest_page, 'html.parser')
+        time_message_box = contest_soup.find('div', attrs={'class': 'message'})
+        print contest_name.text
+        print time_message_box.find('h2').text
+        print contest.attrs['onclick'][17:-1]
--- konkurs_trojmiasto_pl.py
+++ konkurs_trojmiasto_pl.py
@@ -7,49 +7,48 @@ This is a temporary script file.
 import urllib2
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 import time
 from functions import to_utf
+from functions import list_contests
 #from datetime import datetime
 
 main_address = "https://konkursy.trojmiasto.pl/"
     
+
+
 no_contests = True
 while(no_contests):
     main_page = urllib2.urlopen(main_address)   #time: ~0.4s
-    before = time.time()
     soup = BeautifulSoup(main_page, 'html.parser')  #time: ~0.2s
-    after = time.time()
     upcoming_contests_soup = soup.find('div', attrs={'class':'upcoming-contests'})
-    upcoming_contests = upcoming_contests_soup.find_all('li', attrs={'class':' item'})
-    upcoming_contests.extend(upcoming_contests_soup.find_all('li', attrs={'class':' first_in_row item'}))
-    number_of_contests = len(upcoming_contests)
-    
-    if (number_of_contests < 1):
-        print ("no contests available", time.localtime()[3:6])
-        time.sleep(60)        
-    else:
-        no_contests = False
-        print ("contest found", time.localtime()[3:6])
-    #print (after - before)
+    if (upcoming_contests_soup):
+        print("something wrogng")
+        upcoming_contests = upcoming_contests_soup.find_all('li', attrs={'class':' item'})
+        upcoming_contests.extend(upcoming_contests_soup.find_all('li', attrs={'class':' first_in_row item'}))
+    #number_of_contests = len(upcoming_contests)
 
-print (number_of_contests, ' contest(s) available')
-for contest in upcoming_contests:
-    contest_name = contest.find('p', attrs={'class':'name'})
-    contest_address = contest.attrs['onclick'][17:-1]  #TODO add possibility of contest choice
-    contest_page = urllib2.urlopen(contest_address)
-    contest_soup = BeautifulSoup(contest_page, 'html.parser')
-    time_message_box = contest_soup.find('div', attrs={'class': 'message'})
-    print contest_name.text
-    print time_message_box.find('h2').text
-    print contest.attrs['onclick'][17:-1]
+    current_contests_soup = soup.find('div', attrs={'class':'upcoming-contests'})
+    if (current_contests_soup):
+        print("something wrogng")
+        current_contests = current_contests_soup.find_all('li', attrs={'class':' item'})
+        current_contests.extend(current_contests_soup.find_all('li', attrs={'class':' first_in_row item'}))
+    
+        if (len(current_contests) > 0):
+            no_contests = False
+            print ("contest found", time.localtime()[3:6])
+    else:
+        print ("no contests available", time.localtime()[3:6])
+        #TODO handle upcoming contests
+        time.sleep(60)        
+
+print ((len(upcoming_contests) + len(current_contests)), ' contest(s) available')
+
+list_contests(upcoming_contests)    #TODO add possibility of contest choice
+list_contests(current_contests)
 
         
-#    current_contests_soup = soup.find('div', attrs={'class':'upcoming-contests'})
-#    current_contests = current_contests_soup.find_all('li', attrs={'class':' item'})
-#    current_contests.extend(current_contests_soup.find_all('li', attrs={'class':' first_in_row item'}))
-#    number_of_contests = len(current_contests)
-#possible to find all contests by id:actual (2 seperate divs) 
 
 ''' #TODO only when there's at least 1 open contest
 contest_address = contests[1].attrs['onclick'][17:-1]  #TODO add possibility of contest choice
@@ -68,9 +67,14 @@ print(question.text)
 
 
 #TODO check if the contest is open
-''' #TODO only when contest is online
-driver = webdriver.Firefox( )
-driver.get(contest_address)
+options = Options()
+#options.headless = True
+#driver = webdriver.Firefox(options=options )
+driver = webdriver.Firefox()
+ #TODO only when contest is online
+#driver.get(contest_address)
+driver.get("https://google.com")
+'''
 elem = driver.find_element_by_name("answer")
 elem.clear()
 #elem.send_keys("33")
